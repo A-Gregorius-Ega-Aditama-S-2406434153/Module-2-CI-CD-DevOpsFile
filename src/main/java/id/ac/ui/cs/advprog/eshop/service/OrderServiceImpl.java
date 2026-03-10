52f092c02f4a341be0132b4e080c6371b7c8fc81
Ega Sudjali <egasudjali1989@gmail.com>
2026-03-10 13:11:45 +0700

    [RED] Add tests for OrderServiceImpl

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
@@ -0,0 +1,37 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order createOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order updateStatus(String orderId, String status) {
+        return null;
+    }
+
+    @Override
+    public Order findById(String orderId) {
+        return null;
+    }
+
+    @Override
+    public List<Order> findAllByAuthor(String author) {
+        return null;
+    }
+}
